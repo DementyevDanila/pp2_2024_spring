71c73d907a7322e9b6219321b230b857e9451887
DementyevDanila <pokamolodoy2006@gmail.com>
2024-01-20 00:25:37 +0600

    different types of vars and how work with them

---
+++ lab1/variable_types.py
@@ -0,0 +1,13 @@
+x = int(3) # x will be 3
+y = str(3) # y will be '3'
+z = float(3) # z will be 3.0
+
+
+print(x,  y,  z)
+
+p = 5
+name = "John"
+name_2 = 'John' # same with "John"
+print(type(p))
+print(type(name))
+print(type(name_2))
