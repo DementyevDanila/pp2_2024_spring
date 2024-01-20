5875bf9954cdc7eb7c227ecc82f9d493c4bd67ba
DementyevDanila <pokamolodoy2006@gmail.com>
2024-01-20 12:16:48 +0600

    added new file

---
+++ lab1/variabe_initiaisation.py
@@ -0,0 +1,15 @@
+x, y, z = "First", "Second", "Third"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+cars = ["bmw", "audi", "toyota"]
+x, y, z = cars
+print(x)
+print(y)
+print(z)
