e03c508e45cacd8830c7f38c7b042a6e9181ba73
DementyevDanila <pokamolodoy2006@gmail.com>
2024-01-20 12:27:13 +0600

    types of output

---
+++ lab1/variable_HowToOutput.py
@@ -0,0 +1,24 @@
+x = "I love Python"
+print(x)
+
+x = "I"
+y = "love"
+z = "Python"
+print(x, y, z)
+
+x = "I "
+y = "love " # space after "love " and "I " is important, because without thems answer wil be IlovePython
+z = "Python"
+print(x + y + z)
+
+x = 5
+y = 10
+print(x + y)
+
+x = 1
+y = "Alexander"
+# print(y + x) inpossible. because y and x have the different types of data
+
+#but this method will give us what we expect
+print (y, x)
+
