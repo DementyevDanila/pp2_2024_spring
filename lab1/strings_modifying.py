b48f8635c45220c40c015c84651c1926ad8bf4c0
DementyevDanila <pokamolodoy2006@gmail.com>
2024-01-21 15:58:52 +0600

    string modifying

---
+++ lab1/strings_modifying.py
@@ -0,0 +1,15 @@
+a = "Hello, World!"
+print(a.upper()) # HELLO, WORLD!
+
+a = "Hello, World!"
+print(a.lower()) # hello, world!
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!" - without spaces 
+
+a = "Hello, World!"
+print(a.replace("H", "J")) #Jello, World!
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+
