83fd08cd51b7dfd5799d2592ae6149bc42148f89
DementyevDanila <pokamolodoy2006@gmail.com>
2024-01-21 15:29:38 +0600

    same methos as erase in c++

---
+++ lab1/strings_continue.py
@@ -0,0 +1,11 @@
+b = "abcdefghij"
+print(b[2:5]) #starts from 2 ends with 5, but 5th element is not incuded. Also string starts from 0th element
+
+b = "abcdefghij"
+print(b[:5]) #starts from 0 
+
+b = "abcdefghij"
+print(b[2:]) #starts from 2 and goes till the end
+
+b = "abcdefghij"
+print(b[-5:-2]) #counting starts from behind, -5th elemnt is "f" and counter goes to the h(without including "i")
