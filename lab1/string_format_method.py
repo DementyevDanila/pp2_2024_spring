5088dfb5a96ba309dafbe3bc9a0be50283c474c4
DementyevDanila <pokamolodoy2006@gmail.com>
2024-01-21 16:26:58 +0600

    format method to modif string by dif types of vars

---
+++ lab1/string_format_method.py
@@ -0,0 +1,16 @@
+
+age = 33
+txt = "My name is Kira Yoshikage, and I am {}"
+print(txt.format(age)) #The format() method takes the passed arguments, formats them, and places them in the string where the placeholders {} are
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price)) #The format() method takes unlimited number of arguments, and are placed into the respective placeholders
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price)) #You can use index numbers {0} to be sure the arguments are placed in the correct placeholders
