eb578e2661cb02baf779801dc45c905d83d5081a
DementyevDanila <pokamolodoy2006@gmail.com>
2024-01-21 16:37:14 +0600

    illegal chars

---
+++ lab1/string_ecape_character.py
@@ -0,0 +1,15 @@
+#I cant write txt = "We are the so-called "Vikings" from the north.", because "" int txt are illegal
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
+'''
+\'	Single Quote
+\"  Double Quote	
+\\	Backslash	
+\n	New Line	
+\r	Carriage Return	
+\t	Tab	
+\b	Backspace	
+\f	Form Feed	
+\ooo	Octal value	
+\xhh	Hex value
+'''
