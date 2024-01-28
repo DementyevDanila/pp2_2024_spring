ceee9390818650ded3e32750a59f880e647c967e
DementyevDanila <pokamolodoy2006@gmail.com>
2024-01-28 22:52:47 +0600

    cout tuple

---
+++ lab2/access_to_tuple.py
@@ -0,0 +1,11 @@
+tup = ("Leonardo", "Mikki", "Raf", "Don")
+print(tup[1])
+print(tup[-1])
+print(tup[0:2])
+print(tup[:3])
+print(tup[3:])
+print(tup[-4:-3])
+if "Mikki" in tup:
+    print("The Last Ronin is here...nobody forgoten")
+else:
+    print("He is eating pizza, just wait")
