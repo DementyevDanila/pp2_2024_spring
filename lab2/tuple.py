9ce5a9a587b51e24c2ad4375a603def79589f0ae
DementyevDanila <pokamolodoy2006@gmail.com>
2024-01-28 21:09:32 +0600

    tuple base

--- lab2/tuple.py
+++ lab2/tuple.py
@@ -1,5 +1,21 @@
 # Tuples are used to store multiple items in a single variable.
 # Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# Tuples are unchangeable, meaning that we cannot change, add or remove items after the tuple has been created.
 
 fruits_but_tuple = ("apple", "banana", "cherry")
 print(fruits_but_tuple)
+
+more_fruits = ("apple", "banana", "cherry", "apple", "banana")
+print(more_fruits)
+print(len(more_fruits))
+
+new_tuple = ("apple",) # to create tule with 1 element I must use , after it
+print(type(new_tuple))
+
+tuple5 = ("apple") # string
+print(type(tuple5)) 
+
+tuple1 = ("abc", 34, True, 40, "male") # tupple can consist any type of data
+
+aboba = tuple(("apple", "banana", "cherry")) 
+print(aboba)
