6409512c5062e543118418a6e7397af0d931051c
DementyevDanila <pokamolodoy2006@gmail.com>
2024-01-29 20:21:47 +0600

    set updates

---
+++ lab2/add_items_to_set.py
@@ -0,0 +1,12 @@
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange") #method to add elements into set
+
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+mylist = ["kiwi", "orange"]
+
+thisset.update(tropical) # it possible to add one set to another 
+thisset.update(mylist) # it possible to add any type of containers to set
+print(thisset)
