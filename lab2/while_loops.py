eb3e580362072182f8329ff6a22b8513c97b954a
DementyevDanila <pokamolodoy2006@gmail.com>
2024-01-29 22:45:49 +0600

    while loops

---
+++ lab2/while_loops.py
@@ -0,0 +1,25 @@
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+print(' ')
+p = 10
+while p != 3:
+    print(p)
+    if p == 4:
+        break
+    p -= 1
+print(' ')
+j = 0
+while j!= 10:
+    j += 1
+    if j % 2 == 0:
+        continue
+    print(j)
+print(' ')
+k = 1
+while k < 6:
+  print(k)
+  k += 1
+else:
+  print("k is no longer less than 6")
