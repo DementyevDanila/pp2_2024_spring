49aa896d0d0fb33302b383bad8f560aac347e1a7
DementyevDanila <pokamolodoy2006@gmail.com>
2024-01-29 19:57:44 +0600

    set access

---
+++ lab2/access_to_set.py
@@ -0,0 +1,6 @@
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+    print(x)
+# because items in set dont have indexes we cantfind it by i
+thisset = {"apple", "banana", "cherry"}
+print("banana" in thisset)
