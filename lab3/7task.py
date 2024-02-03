bacfd1379a3af65c4e2541200a40422bc4b508b5
DementyevDanila <pokamolodoy2006@gmail.com>
2024-02-03 18:22:57 +0600

    yeah

--- lab3/7task.py
+++ lab3/7task.py
@@ -1,21 +1,20 @@
 def has_33(num):
     cnt = 0
+    chk = False
     for x in num:
         if x == 3:
             cnt += 1
             if cnt == 2: 
-                return True
+                print("True")
+                chk = True
                 break
         else:
             cnt = 0
-        
+    if (chk == False):
+        print("False")
+
 num = input()
 num = num.split()
-print(type(num))
 newlist = list(map(int, num))
-print(type(newlist))
 
-if has_33(newlist):
-    print("True")
-else:
-    print("False")
+has_33(newlist)
