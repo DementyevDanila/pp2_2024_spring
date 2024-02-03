59e366b120bde8a38b94126420637c6a976f56e5
DementyevDanila <pokamolodoy2006@gmail.com>
2024-02-03 18:25:10 +0600

    yeah

--- lab3/8task.py
+++ lab3/8task.py
@@ -1,21 +1,20 @@
 def spy_among_us(p):
     cnt = 0
+    che = False
     for x in p:
         if x == 0:
             cnt += 1
         if (x == 7 and cnt == 2): 
-            return True
+            print("True")
+            che = True
             break
         if x != 0:
             cnt = 0
-
+    if (che == False):
+        print("False")
 num = input()
 num = num.split()
 newlist = list(map(int, num))
 
-if spy_among_us(newlist):
-    print("True")
-else:
-    print("False")
-
+spy_among_us(newlist)
         
