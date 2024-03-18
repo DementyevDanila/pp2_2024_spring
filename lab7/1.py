2d129ce868e28dd9b6bf59a0ee0d8d36acfc6124
DementyevDanila <pokamolodoy2006@gmail.com>
2024-03-19 01:41:21 +0500

    clock

---
+++ lab7/1.py
@@ -0,0 +1,44 @@
+import pygame
+import time
+
+pygame.init()
+
+width, height = 1200, 800
+screen = pygame.display.set_mode((width, height))
+
+clock_image = pygame.image.load(r"C:\Users\pokam\Desktop\My education\pp2\lab7\Images\mainclock.png")
+sec_hand_image = pygame.image.load(r"C:\Users\pokam\Desktop\My education\pp2\lab7\Images\leftarm.png")
+min_hand_image = pygame.image.load(r"C:\Users\pokam\Desktop\My education\pp2\lab7\Images\rightarm.png")
+
+done = False
+
+rect1 = clock_image.get_rect()
+rect1.center = screen.get_rect().center
+
+while not done:
+    screen.fill((255, 255, 255))
+    screen.blit(clock_image, rect1)
+    
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    current_time = time.localtime()
+    sec_angle = -(current_time.tm_sec * 360 / 60)
+    min_angle = -(current_time.tm_min * 360 / 60)
+
+    sec_hand = pygame.transform.rotate(sec_hand_image, sec_angle)
+    min_hand = pygame.transform.rotate(min_hand_image, min_angle)
+
+    rect2 = sec_hand.get_rect()
+    rect2.center = screen.get_rect().center
+    screen.blit(sec_hand, rect2)
+    
+    rect3 = min_hand.get_rect()
+    rect3.center = screen.get_rect().center
+    screen.blit(min_hand, rect3)
+
+    pygame.display.update()
+
+pygame.quit()
